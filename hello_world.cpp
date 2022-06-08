31803acb299de850cfc89ff6221e23e4be065e72
vbifxvj <ceyko.m@mail.ru>
2022-06-08 19:45:50 +0300

    mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
 
-
-
-int main(int count, char** strings){
-	
-	std::string name;
-	std::cout << "hello world " << name << std::endl;
-	return 0;
+int
+main(int count, char** strings)
+{
+  std::string name;
+  std::cout << "hello world " << name << std::endl;
+  return 0;
 }
 
